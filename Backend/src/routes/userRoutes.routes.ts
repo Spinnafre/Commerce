f22217f58b67b0f29963b06bd7acb02747a160ff
João Victor Sampaio <70032581+jvsampaios@users.noreply.github.com>
2022-01-21 15:45:29 -0300

    Add files via upload

---
+++ Backend/src/routes/userRoutes.routes.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import CreateUserController from '../modules/accounts/UseCases/CreateUser'
+import ShowUsersController from '../modules/accounts/UseCases/ShowUsers'
+import ShowUserController from '../modules/accounts/UseCases/ShowUser'
+import UpdateUserController from '../modules/accounts/UseCases/UpdateUser'
+import DeleteUserController from '../modules/accounts/UseCases/DeleteUser'
+import { authorizationUser } from "../middlewares/ensureAuthenticated";
+import { userIsAdmin } from "../middlewares/ensureAdmin";
+
+
+const userRouter=Router()
+
+
+userRouter.post('/user',(req, res) => {
+    return CreateUserController().handle(req, res)
+})
+userRouter.patch('/user',authorizationUser,(req, res) => {
+    return UpdateUserController().handle(req, res)
+})
+userRouter.get('/user',authorizationUser,userIsAdmin,(req, res) => {
+    return ShowUsersController().handle(req, res)
+})
+userRouter.get('/user/:id',authorizationUser,userIsAdmin,(req, res) => {
+    return ShowUserController().handle(req, res)
+})
+userRouter.delete('/user/:id',authorizationUser,userIsAdmin,(req, res) => {
+    return DeleteUserController().handle(req, res)
+})
+
+
+export {userRouter}
